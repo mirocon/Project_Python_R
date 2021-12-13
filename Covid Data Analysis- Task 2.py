b8568addcf49b7d7c6914d94f09aac7ceb6412e3
GiorgiaLeonardi <93827305+GiorgiaLeonardi@users.noreply.github.com>
2021-12-13 17:09:39 +0100

    Update Covid Data Analysis- Task 2.py

--- Covid Data Analysis- Task 2.py
+++ Covid Data Analysis- Task 2.py
@@ -27,3 +27,35 @@ df_covid_death = pd.concat([pd.Series(v['deaths'].tolist(), name=k) for k, v in 
 df_covid_death.fillna(0, inplace=True) #here I fill the null values as 0 (some low income countries have NaN values in certain dates)
 df_covid_death.index = pd.to_datetime(df_covid_death.index, dayfirst=True)
 df_covid_death.index = df_covid_death.index.sort_values()
+
+### million inhabitants datframe
+df_covid_million = pd.concat([pd.Series(v['cases_per_million_inhabitants'].tolist(), name=k) for k, v in grouper], axis=1).set_index(data["date"][1:351])
+df_covid_million.fillna(0, inplace=True) #here I fill the null values as 0 (some low income countries have NaN values in certain dates)
+df_covid_million.index = pd.to_datetime(df_covid_million.index, dayfirst=True)
+df_covid_million.index = df_covid_million.index.sort_values()
+
+###create dataframe cumulative cases
+df_cases_cum = pd.concat([pd.Series(v['cases_cum'].tolist(), name=k) for k, v in grouper], axis=1).set_index(data["date"][1:351])
+df_cases_cum.fillna(0, inplace=True) #here I fill the null values as 0 (some low income countries have NaN values in certain dates)
+df_cases_cum.index = pd.to_datetime(df_cases_cum.index, dayfirst=True)
+df_cases_cum.index = df_cases_cum.index.sort_values()
+
+### plot data cases
+def plot_data_cases(df, title="COVID-19 Cases"):
+    df.plot(title=title, fontsize=7)
+    plt.xlabel("Dates")
+    plt.ylabel("Cases")
+    plt.xticks( rotation=45)
+    plt.tick_params(axis='y', labelsize=7)
+    plt.tight_layout()
+    plt.show()
+
+### plot data deaths
+def plot_data_death(df, title="COVID-19 Death"):
+    df.plot(title=title, fontsize=7)
+    plt.xlabel("Dates")
+    plt.ylabel("Deaths")
+    plt.xticks( rotation=45)
+    plt.tick_params(axis='y', labelsize=7)
+    plt.tight_layout()
+    plt.show()
